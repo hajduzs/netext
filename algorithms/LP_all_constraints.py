5c15e382fb7c9756ab4d4170d6439b01d0ab6e9b
Zsombor Hajdú <hajdu.zsombor.laszlo@gmail.com>
2020-02-03 16:46:22 +0100

    slight reformatting+1

--- algorithms/LP_all_constraints.py
+++ algorithms/LP_all_constraints.py
@@ -1,5 +1,6 @@
 from NetworkModels.ConstarintGraph import ConstraintGraph
 from libs.Wrappers.PathPlanner import PathPlanner
+import helper_functions as func
 
 import algorithms.algoritmhs_helper_functions as a_func
 import Utilities.Logging as logging
@@ -31,9 +32,21 @@ def lp_all_constraints(TOPOLOGY, DZL, BPD, R, g_r_path):
 
     for n, d in BPD.graph.nodes(data=True):
 
-        ids = a_func.get_ids_to_avoid(n, d, BPD, TOPOLOGY)
-        if ids is None:
-            continue
+        if d["bipartite"] == 1:
+            return None
+
+        # get ccordinates
+        pnodes = d["vrtx"].edge
+        pi = func.get_coords_for_node(pnodes[0], TOPOLOGY)
+        pg = func.get_coords_for_node(pnodes[1], TOPOLOGY)
+
+        # get adjacent danger zones
+        neigh_cuts = [v for v in BPD.graph.nodes if BPD.graph.has_edge(n, v)]
+        d["neigh"] = len(neigh_cuts)
+        ids = set()
+        for c in neigh_cuts:
+            ids.update(BPD.return_ids_for_cut(c))
+        ids = list(ids)
 
         i_subsets = itertools.chain.from_iterable(itertools.combinations(ids, r) for r in range(1, len(ids) + 1))
         for subs in i_subsets:
